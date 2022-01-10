04895dbf0396fb51822a5441c890e729aab9b2ea
Singh <703302838@genpact.com>
2022-01-10 13:58:59 +0530

    servlet asse

---
+++ main/java/com/org/Login.java
@@ -0,0 +1,102 @@
+package com.org;
+
+
+
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
+
+
+/**
+* Servlet implementation class Login
+*/
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+private static final long serialVersionUID = 1L;
+
+
+
+/**
+* @see HttpServlet#HttpServlet()
+*/
+public Login() {
+super();
+// TODO Auto-generated constructor stub
+}
+
+
+
+
+
+/**
+* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+*/
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+// TODO Auto-generated method stub
+//response.getWriter().append("Served at: ").append(request.getContextPath());
+
+
+
+response.setContentType("text/html");
+PrintWriter out = response.getWriter();
+String username = request.getParameter("username");
+String password = request.getParameter("password");
+try {
+Class.forName("com.mysql.cj.jdbc.Driver");
+System.out.println("connection ready");
+Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/register","root","S@njay123");
+System.out.println(con);
+
+
+
+PreparedStatement ps = con.prepareStatement("select * from registeruser where name=? and pass=?");
+ps.setString(1, username);
+ps.setString(2, password);
+
+
+
+System.out.println(ps.execute());
+ResultSet rs=ps.executeQuery();
+while(rs.next()) {
+System.out.println(rs.getString(2));
+
+
+
+}
+//System.out.println("login "+ rs);
+out.print("Login success");
+}catch(Exception e){
+System.out.println(e);
+}
+out.close();
+
+
+
+
+}
+
+
+
+
+
+}
---
+++ main/java/com/org/Register.java
@@ -0,0 +1,72 @@
+package com.org;
+
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+* Servlet implementation class Register
+*/
+@WebServlet("/Register")
+public class Register extends HttpServlet {
+private static final long serialVersionUID = 1L;
+
+
+
+/**
+* @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+*/
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+// TODO Auto-generated method stub
+response.setContentType("text/html");
+PrintWriter out = response.getWriter();
+
+String n=request.getParameter("userName");
+String p=request.getParameter("userPass");
+String e=request.getParameter("userEmail");
+String c=request.getParameter("userCountry");
+
+try{
+Class.forName("com.mysql.cj.jdbc.Driver");
+Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/register","root","S@njay123");
+
+
+
+PreparedStatement ps=con.prepareStatement("insert into registeruser values(?,?,?,?)");
+
+
+
+ps.setString(1,n);
+ps.setString(2,p);
+ps.setString(3,e);
+ps.setString(4,c);
+
+
+
+int i=ps.executeUpdate();
+if(i>0)
+out.print("You are successfully registered...");
+
+
+
+System.out.println("test");
+}catch (Exception e2) {System.out.println(e2);}
+}
+
+
+
+}
